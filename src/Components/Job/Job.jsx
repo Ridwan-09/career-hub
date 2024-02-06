759db4f597ab0f08f92a12c2793dd3983ce4e300
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 18:44:03 +0600

    career

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,4 +1,5 @@
 import { MdLocationOn } from "react-icons/md";
+import { FaBangladeshiTakaSign } from "react-icons/fa6";
 
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
@@ -12,8 +13,9 @@ const Job = ({ job }) => {
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
                 </div>
-                <div>
-                    <h2 className="flex"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
+                <div className="mt-4 flex">
+                    <h2 className="flex mr-4"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
+                    <h2 className="flex"> <FaBangladeshiTakaSign className="text-2xl"></FaBangladeshiTakaSign> {salary}</h2>
                 </div>
                 <div className="card-actions">
                     <button className="btn btn-primary">View Details</button>
