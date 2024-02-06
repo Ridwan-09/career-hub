bfa03308dec0c897d70ed667fe2e1bd6a9165ce7
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 12:26:22 +0600

    career

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import CategoryList from "../CategoryList/CategoryList";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <CategoryList></CategoryList>
             <h2>This is home</h2>
         </div>
     );
