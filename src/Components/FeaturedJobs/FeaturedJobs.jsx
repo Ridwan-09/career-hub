ad498f11b2ebea1ebe02245e83e236f27065837b
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 13:19:34 +0600

    career

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
+
 const FeaturedJobs = () => {
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect( () => {
+        fetch('jobs.json')
+        .then(res => res.json())
+        .then(data => setJobs(data));
+    }, [])
+
     return (
         <div>
             <div className="text-center">
                 <h2 className="text-5xl">Featured Jobs</h2>
+                <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div>
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
         </div>
     );
 };
