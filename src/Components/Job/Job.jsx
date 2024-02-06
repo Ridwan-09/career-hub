14eb02dd375a698750a10d6d0890574b9f3adf36
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 16:01:24 +0600

    career

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,13 +1,17 @@
 const Job = ({ job }) => {
-    const { logo } = job;
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
         <div className="card bg-base-100 shadow-xl">
             <figure><img src={logo} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4">{remote_or_onsite}</button>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4">{job_type}</button>
+                </div>
+                <div className="card-actions">
+                    <button className="btn btn-primary">View Details</button>
                 </div>
             </div>
         </div>
