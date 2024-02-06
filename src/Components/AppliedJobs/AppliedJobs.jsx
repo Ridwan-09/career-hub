64476611d4fa1a5e4bfea940ee9e45241d20035e
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-07 00:26:46 +0600

    career

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -6,6 +6,21 @@ const AppliedJobs = () => {
     const jobs = useLoaderData();
 
     const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
+
+    const handleJobsFilter = filter => {
+        if(filter === 'all'){
+            setDisplayJobs(appliedJobs);
+        }
+        else if (filter === 'remote'){
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs);
+        }
+        else if (filter === 'onsite'){
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onsiteJobs);
+        }
+    }
 
     useEffect(() => {
         const storedJobIds = getStoredJobApplication();
@@ -19,9 +34,9 @@ const AppliedJobs = () => {
                 }
             }
             setAppliedJobs(jobsApplied);
-
+            setDisplayJobs(jobsApplied);
         }
-    }, [])
+    }, [jobs])
 
     return (
         <div>
@@ -38,7 +53,7 @@ const AppliedJobs = () => {
 
             <ul>
                 {
-                    appliedJobs.map(job => <li key={job.id}>
+                    displayJobs.map(job => <li key={job.id}>
                         <span>{job.job_title} {job.company_name}: {job.remote_or_onsite} </span>
                     </li>)
                 }
