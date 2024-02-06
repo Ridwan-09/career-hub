9c84a6478bd69f4aaee4d6c941ad376f441c9842
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 11:27:19 +0600

    career

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,9 @@ const Header = () => {
 
     const links = <>
         <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink>Jobs</NavLink></li>
+        <li><NavLink to="/jobs">Jobs</NavLink></li>
         <li><NavLink to="/applied">Applied Jobs</NavLink></li>
+        <li><NavLink to="/statistics">Statistics</NavLink></li>
         <li><NavLink to="/blogs">Blogs</NavLink></li>
     </>
 
