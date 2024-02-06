c89af8d48d0161785c7035e3be237fe7dafb7e6b
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 16:04:08 +0600

    career

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -7,8 +7,8 @@ const Job = ({ job }) => {
                 <h2 className="card-title">{job_title}</h2>
                 <p>{company_name}</p>
                 <div>
-                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4">{remote_or_onsite}</button>
-                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4">{job_type}</button>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
                 </div>
                 <div className="card-actions">
                     <button className="btn btn-primary">View Details</button>
