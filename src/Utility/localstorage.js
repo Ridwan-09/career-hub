60f1c591b0dd6c737a8b211874bfdfe16f88002c
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 20:50:00 +0600

    career

--- src/Utility/localstorage.js
+++ src/Utility/localstorage.js
@@ -7,7 +7,11 @@ const getStoredJobApplication = () => {
 }
 
 const saveJobApplication = id => {
-
+    const storedJobApplications = getStoredJobApplication();
+    const exists = storedJobApplications.find(jobId => jobId === id);
+    if(!exists){
+        storedJobApplications.push(id);
+    }
 }
 
 export {saveJobApplication}
