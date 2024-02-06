721ab9ad771bb8cb645e610046fa8aadc6c13c7e
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 17:19:57 +0600

    career

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,3 +1,5 @@
+import { MdLocationOn } from "react-icons/md";
+
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
@@ -9,6 +11,9 @@ const Job = ({ job }) => {
                 <div>
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
+                </div>
+                <div>
+                    <h2 className="flex"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
                 </div>
                 <div className="card-actions">
                     <button className="btn btn-primary">View Details</button>
