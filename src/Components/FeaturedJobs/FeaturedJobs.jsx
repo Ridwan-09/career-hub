f37936b3b4201cf006c9c2c9e3fdf9f47a0a3a8d
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-06 12:45:59 +0600

    career

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,7 @@ const FeaturedJobs = () => {
             <div className="text-center">
                 <h2 className="text-5xl">Featured Jobs</h2>
             </div>
+            <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
         </div>
     );
 };
